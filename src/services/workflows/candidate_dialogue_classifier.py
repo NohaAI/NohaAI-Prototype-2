64b58f324d57c9c9dae95bbb92b43bb7e6216eff
adhirathee <bytemonk@gmail.com>
2025-03-08 14:41:11 +0530

    refactor: fix candidate_dialogue_classifier function, reduced the num of input/output arguments, session_state fields are update correctly and now returns on label_class1 and rationale

--- src/services/workflows/candidate_dialogue_classifier.py
+++ src/services/workflows/candidate_dialogue_classifier.py
@@ -4,26 +4,31 @@ import json
 from src.services.llm.prompts.classify_candidate_dialogue_prompt import classify_candidate_dialogue_prompt_template
 logger = logger.get_logger(__name__)
 
-async def classify_candidate_dialogue(bot_dialogue, candidate_dialogue,chat_history):
+async def classify_candidate_dialogue(session_state, chat_history):
+    logger.info("\n\n\n>>>>>>>>>>>FUNCTION [classify_candidate_dialogue] >>>>>>>>>>>>>>>>>>>>>>>>>>")
+
     classify_candidate_dialogue_prompt=classify_candidate_dialogue_prompt_template()
     llm_model = llm_service.get_openai_model()
-    distilled_dialogue = ""
     classify_candidate_dialogue_chain=(classify_candidate_dialogue_prompt|llm_model)
-    logger.info(f"INPUTS TO CLASSIFY CANDIDATE DIALOGUE : \n bot_dialogue: {bot_dialogue} \n candidate_dialogue : {candidate_dialogue} \n chat_history : {chat_history} \n distilled_dialogue : {distilled_dialogue}")
 
-    llm_inputs = {'bot_dialogue': bot_dialogue,
-                'candidate_dialogue': candidate_dialogue,
+    llm_inputs = {'bot_dialogue': session_state["bot_dialogue"],
+                'candidate_dialogue': session_state["candidate_dialogue"],
                 'chat_history': chat_history,
-                'distilled_candidate_dialogue': distilled_dialogue 
+                'distilled_candidate_dialogue': chat_history["distilled_candidate_dialogue"] 
                 }
 
     classification_response = await classify_candidate_dialogue_chain.ainvoke(llm_inputs)
     classification_content = json.loads(classification_response.content)
-    logger.info(f"CLASSIFICATION CONTENT FOR CANDIDATE DIALOGUE : {classification_content} \n")
-    #TODO: keep a if-else or try except to verify LLM Response
-    if len(classification_content) == 1:
-        classification_content.append("EMPTY RATIONALE FROM LLM")
-    label = classification_content[0]
+
+    logger.info(f"\nOUTPUTS FROM CLASSIFY CANDIDATE DIALOGUE LLM : {classification_content} \n")
+   
+    label_class1 = classification_content[0]
     rationale = classification_content[1]
-    distilled_dialogue = classification_content[2]
-    return label, rationale, distilled_dialogue #where 0, 1, 2 are label, rationale and distilled dialoguer respectively     
+    distilled_candidate_dialogue = classification_content[2]
+
+     # update the respective fields in the session_state and chat_history
+    session_state['candidate_dialogue'] = distilled_candidate_dialogue  # replaces the original candidate_dialogue with the refined version to carry forward hereafter
+    chat_history[-1]['distilled_candidate_dialogue'] = distilled_candidate_dialogue # adds the distilled version for update in DB to the recentmost dict record in the chat_history list
+
+    logger.info(">>>>>>>>>>>FUNCTION EXIT [classify_candidate_dialogue] >>>>>>>>>>>>>>>>>>>>>>>>>>\n\n")
+    return label_class1, rationale, distilled_dialogue #where 0, 1, 2 are label, rationale and distilled dialoguer respectively     
