b66b3c8e36519ac79f2eb0f2401753ddb4d6053e
adhirathee <bytemonk@gmail.com>
2025-03-08 10:45:58 +0530

    refactor: modify few chat history defaults and add few assessment default constants

--- src/config/constants.py
+++ src/config/constants.py
@@ -1,6 +1,6 @@
 # Application-wide constants
 
-# INTERVIEW THRESHOLDS
+# INTERVIEW THRESHOLDS ############################################################
 THRESHOLD_MAX_TURNS = 10
 THRESHOLD_SCORE = 5.0
 THRESHOLD_MAX_CONTIGUOUS_NON_TECHNICAL_GUARDRAIL_COUNT = 4
@@ -9,8 +9,9 @@ THRESHOLD_MAX_CONTIGUOUS_TECHNICAL_GUARDRAIL_COUNT = 4
 THRESHOLD_COMPLEXITY = 1
 THRESHOLD_TOTAL_NUMBER_OF_QUESTIONS = 2
 
-# BOT CANNED DIALOGUES
-GREETING_SUFFIX = "I am Noha! I am your interviewer today. We have planned a data structures and algorithms interview with you, are you good to go?"
+# BOT CANNED DIALOGUES ############################################################
+GREETING_SUFFIX = "Ready?"
+GREETING_SUFFIX_ORIG = "I am Noha! I am your interviewer today. We have planned a data structures and algorithms interview with you, are you good to go?"
 QUESTION_SOLVED = "Since you have solved this question, can you now start writing code for it?"
 GUARDRAIL_TRIGGERED_QUESTIONS_LEFT = "It seems there is a lack of clarity. Let us move on to the next question : "
 GUARDRAIL_TRIGGERED_NO_QUESTIONS_LEFT = "It seems there is a lack of clarity. Let us conclude here."
@@ -19,7 +20,7 @@ MAX_TURNS_TRIGGERED_QUESTIONS_LEFT = "So far so good, let us move on to the next
 MAX_TURNS_TRIGGERED_NO_QUESTIONS_LEFT = "We appreciate your effort on the problem! Now, can you code it for us? Let us know when you're ready."
 ALL_QUESTIONS_ANSWERED = "Thank you for your participation"
 
-# LIST OF LABELS
+# LIST OF LABELS ################################################################
 TECHNICAL_LABELS= ['technical', 'clarification(specific)'] #todo: refine and suitably rename the label 'technical' later
 NON_TECHNICAL_LABELS= [
     "illegible", "irrelevant", "interview_inquiry", "clarification(open)", "confirmation", "negation", "request(new_question)", "request(termination)", "request(proceed)", "request(break)", "disregard", 
@@ -28,11 +29,12 @@ NON_TECHNICAL_LABELS= [
 TECHNICAL_LABELS_TO_BE_EVALUATED = ['doubt(problem)', 'solution']
 TECHNICAL_LABELS_NOT_TO_BE_EVALUATED= ['doubt(concept)']
 
-# SESSION INITIALIZATION DEFAULTS
+# SESSION INITIALIZATION DEFAULTS ################################################
 DEF_INTERVIEW_ID = None  # Set dynamically
+DEF_BOT_DIALOGUE = None  # Set dynamically
+DEF_CANDIDATE_DIALOGUE = None # Set dynamically
 DEF_TURN_NUMBER = 0
 DEF_CONSECUTIVE_TERMINATION_REQUEST_COUNT = 0
-DEF_BOT_DIALOGUE = None  # Set dynamically
 DEF_GUARDRAIL_COUNT = 0
 DEF_CONTIGUOUS_TECHNICAL_GUARDRAIL_COUNT = 0
 DEF_CONTIGUOUS_NON_TECHNICAL_GUARDRAIL_COUNT = 0
@@ -43,10 +45,16 @@ DEF_QUESTIONS_ASKED = []
 DEF_BOT_DIALOGUE_TYPE = "greeting"
 DEF_COMPLEXITY = 1
 
-# CHAT HISTORY DEFAULTS
-DEF_INTERVIEW_ID = None # Set dynamically in initialize
-DEF_QUESTION_ID = None # Set later dynamically in get_next_question function
-DEF_BOT_DIALOGUE_TYPE = 'greeting'
-DEF_BOT_DIALOGUE = None # Set dynamically in initialize
-DEF_CANDIDATE_DIALOGUE = None # Set dynamically from client as response to first greeting message
+# CHAT HISTORY DEFAULTS ###########################################################
+### DEF_INTERVIEW_ID = None # have been already defined in session defaults section
+DEF_QUESTION_ID = None # Set dynamically in initialize
+### DEF_BOT_DIALOGUE = None # have been already defined in session defaults section
+### DEF_BOT_DIALOGUE_TYPE = 'greeting' # have been already defined in session defaults section
+### DEF_CANDIDATE_DIALOGUE = None # Set dynamically from client as response to first greeting message
 DEF_DISTILLED_CANDIDATE_DIALOGUE = None # Set dynamically from candidate_classifier
+
+# ASSESSMENT DEFAULTS ###########################################################
+### DEF_INTERVIEW_ID = None # have been already defined in session defaults section
+### DEF_QUESTION_ID = None # have been already defined in chat history defaults section
+DEF_SCORE = 0.0
+DEF_ASSESSMENT_PAYLOAD = None 
