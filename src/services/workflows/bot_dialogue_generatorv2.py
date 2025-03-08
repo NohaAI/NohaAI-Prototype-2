f9b3c23f77a83491459df9a47297dcf4d82fd2cc
adhirathee <bytemonk@gmail.com>
2025-03-08 18:39:36 +0530

    refactor: change the method signature and mostly all the code to align with the changes; major update

--- src/services/workflows/bot_dialogue_generatorv2.py
+++ src/services/workflows/bot_dialogue_generatorv2.py
@@ -1,48 +1,50 @@
 from src.utils.logger import get_logger
+from src.utils import helper as helper
 from src.services.llm import llm_service
 import json
 from src.services.llm.prompts.bot_dialogue_prompt import bot_dialogue_prompt_template 
 # Initialize logger
 logger = get_logger(__name__)
 
-async def generate_dialogue(class_label, distilled_chat_history, candidate_dialogue, question, answer_evaluation=None,  bot_dialogue=None, rationale=None):
-    # non_technical_class_labels=["illegible", "irrelevant", "clarification(specific)", "clarification(open)", "request(guidance)", "request(termination)", "request(proceed)", "request(break)", "disregard", "illegitimate", "uncertainty"]
-    if not  bot_dialogue:
-         bot_dialogue=""
-    if not rationale:
-        rationale=""
-    if not answer_evaluation:
-        answer_evaluation={}
-   #writing to rationale_logs.txt
-    # generate_dialogue_inputs=f"GENERATE DIALOGUE INPUTS class_label : {class_label} , candidate_dialogue : {candidate_dialogue}, question : {question}, previous_dalogue : { bot_dialogue} rationale : {rationale} \n"
-    # log_file = "rationale_logs.txt"
-    # with open(log_file, "a") as file:
-    #     file.write(generate_dialogue_inputs)
+async def generate_dialogue(session_state, chat_history, assessment, rationale=None):
+    logger.info("\n\n>>>>>>>>>>>FUNCTION [generate dialogue] >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     
+    helper.pretty_log("session_state", session_state)
+    helper.pretty_log("chat_history", chat_history)
+    helper.pretty_log("assessment", assessment)
+    # non_technical_class_labels=["illegible", "irrelevant", "clarification(specific)", "clarification(open)", "request(guidance)", "request(termination)", "request(proceed)", "request(break)", "disregard", "illegitimate", "uncertainty"]
+    if not session_state['solution_classifier_executed']:
+        class_label = session_state['label_class1']
+    else:
+        class_label = session_state['label_class2']
+
     bot_dialogue_prompt=bot_dialogue_prompt_template()
     llm_model = llm_service.get_openai_model()
     bot_dialogue_prompt_chain=(bot_dialogue_prompt|llm_model)
     
-    llm_inputs = {'class': class_label, 
-                'tech_question': question,
-                'bot_dialogue':  bot_dialogue,
-                'candidate_dialogue': candidate_dialogue,
-                'chat_history' : distilled_chat_history,
+    llm_inputs = {'class_label': class_label,
+                'current_question': session_state['current_question'],
+                'bot_dialogue':  session_state['bot_dialogue'],
+                'candidate_dialogue': session_state['candidate_dialogue'],
+                'chat_history' : chat_history,
                 'rationale': rationale,
-                'answer_evaluation' : answer_evaluation
+                'assessment_payload' : assessment[-1]['assessment_payload']
                 }
 
-    bot_dialogue_generator_response=await bot_dialogue_prompt_chain.ainvoke(llm_inputs)
-    
-    bot_dialogue_generator_fallbacks = {"rationale": "LLM DIDNT GENERATE RATIONALE", "next_action": "Pass", "subcriterion": "LLM DIDNT PROVIDE SUBCRITERION"}
-    
-    bot_dialogue_generator_content=json.loads(bot_dialogue_generator_response.content)
-    for key, value in bot_dialogue_generator_fallbacks.items():
-        if key not in bot_dialogue_generator_content:
-            bot_dialogue_generator_content[key] = value
-    dialogue=bot_dialogue_generator_content['response']
-    rationale=bot_dialogue_generator_content['rationale']
-    subcriterion=bot_dialogue_generator_content['subcriterion']
-    next_action=bot_dialogue_generator_content['next_action']
-    return dialogue, rationale, subcriterion, next_action
+    llm_response_bot_dialogue_generator = await bot_dialogue_prompt_chain.ainvoke(llm_inputs)
+    llm_content_bot_dialogue_generator = json.loads(llm_response_bot_dialogue_generator.content)
+
+    helper.pretty_log("CLASSIFY CANDIDATE DIALOGUE LLM OUTPUT", llm_content_bot_dialogue_generator)
+
+
+    bot_dialogue = llm_content_bot_dialogue_generator['response']
+    bot_dialogue_rationale = llm_content_bot_dialogue_generator['rationale']
+    bot_dialogue_causal_subcriterion = llm_content_bot_dialogue_generator['subcriterion']
+    bot_dialogue_next_action = llm_content_bot_dialogue_generator['next_action']
+
+    session_state['bot_dialogue'] = bot_dialogue
+    session_state['next_action'] = bot_dialogue_next_action
+
+    logger.info(">>>>>>>>>>>FUNCTION EXIT [bot_dialogue_generator] >>>>>>>>>>>>>>>>>>>>>>>>>>\n\n")
+    return bot_dialogue_rationale, bot_dialogue_causal_subcriterion
 
