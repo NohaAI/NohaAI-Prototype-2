83d2b9d188219475b65b8b61989743fb09f1bec6
adhirathee <bytemonk@gmail.com>
2025-03-12 19:10:34 +0530

    refactor: replaced bot_dialogue_generator.py with the newer bot_dialogue_generatorv2.py

--- src/services/workflows/bot_dialogue_generator.py
+++ src/services/workflows/bot_dialogue_generator.py
@@ -1,116 +1,51 @@
-from fastapi import FastAPI
-from pydantic import BaseModel, Field
-from datetime import datetime
-import psycopg2
-from psycopg2.pool import SimpleConnectionPool
-from typing import List, Dict, Optional, Union
-import os
-import logging
-from dotenv import load_dotenv
 from src.utils.logger import get_logger
-from src.dao.interview import get_interview_metadata
-from src.dao.exceptions import InterviewNotFoundException
+from src.utils import helper as helper
 from src.services.llm import llm_service
 import json
-from src.dao.criterion import fetch_criteria
 from src.services.llm.prompts.bot_dialogue_prompt import bot_dialogue_prompt_template 
-from src.services.llm.prompts.hint_prompt import hint_prompt_template,hint_prompt_template_if_else,hint_prompt_template_assumption_corner_cases,hint_prompt_template_data_structures,hint_prompt_template_algorithms,hint_prompt_template_time_complexity,hint_prompt_template_space_complexity
 # Initialize logger
 logger = get_logger(__name__)
 
-async def generate_dialogue(label, chat_history, answer, question,answer_evaluation, hint_count, previous_dialogue=None):
-    non_technical_class_labels=["illegible", "irrelevant", "clarification(specific)", "clarification(open)", "request(guidance)", "request(termination)", "request(proceed)", "request(break)", "disregard", "illegitimate", "uncertainty"]
-    if label in non_technical_class_labels:
-        if not previous_dialogue:
-            follow_up_question=""
-        follow_up_question=previous_dialogue
-        bot_dialogue_prompt=bot_dialogue_prompt_template()
-        llm_model = llm_service.get_openai_model()
-        bot_dialogue_prompt_chain=(bot_dialogue_prompt|llm_model)
-        bot_dialogue=await bot_dialogue_prompt_chain.ainvoke({'class': label, 'question': question, 'follow_up_question': follow_up_question, 'answer': answer,'chat_history' : chat_history})
-        return bot_dialogue
+async def generate_dialogue(session_state, chat_history, assessment, rationale=None):
+    logger.info("\n\n>>>>>>>>>>>FUNCTION [generate dialogue] >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    
+    # non_technical_class_labels=["illegible", "irrelevant", "clarification(specific)", "clarification(open)", "request(guidance)", "request(termination)", "request(proceed)", "request(break)", "disregard", "illegitimate", "uncertainty"]
+    if not session_state['solution_classifier_executed']:
+        class_label = session_state['label_class1']
     else:
-    #dialogue generation for specfic technical labels needs to be taken care of 
-        criteria=await fetch_criteria(1)
-        if isinstance(answer_evaluation, str):
-            answer_evaluation = json.loads(answer_evaluation)
-        subcriteria_list=[]
-        for i in range(0,21):
-            subcriterion = list(answer_evaluation["evaluation_results"][i].keys())[0]
-            subcriteria_list.append(subcriterion)
-        hint_prompt=hint_prompt_template()
-        hint_prompt_if_else=hint_prompt_template_if_else()
-        hint_prompt_asc=hint_prompt_template_assumption_corner_cases()
-        hint_prompt_ds=hint_prompt_template_data_structures()
-        hint_prompt_a=hint_prompt_template_algorithms()
-        hint_prompt_tc=hint_prompt_template_time_complexity()
-        hint_prompt_sc=hint_prompt_template_space_complexity()
-        llm_model = llm_service.get_openai_model()
+        class_label = session_state['label_class2']
 
-        logger.info(f"ANSWER EVALUATION {answer_evaluation}")
-        logger.info(f"HINT COUNT AFTER {len(chat_history)} : {hint_count}")
-        
-        assumption_score = answer_evaluation["criteria_scores"][0]          
-        corner_case_score = answer_evaluation["criteria_scores"][1]        
-        data_structures_score = answer_evaluation["criteria_scores"][2]
-        algorithm_score = answer_evaluation["criteria_scores"][3]   
-        time_complexity_score = answer_evaluation["criteria_scores"][4]    
-        space_complexity_score = answer_evaluation["criteria_scores"][5]    
-        #check for counter 
-        if (assumption_score < 0.2 or corner_case_score < 0.2) and hint_count[0] < 2:
-            hint_count[0] += 1
+    bot_dialogue_prompt=bot_dialogue_prompt_template()
+    llm_model = llm_service.get_openai_model()
+    bot_dialogue_prompt_chain=(bot_dialogue_prompt|llm_model)
+    helper.pretty_log("assssment", assessment)
+    llm_inputs = {'class_label': class_label,
+                'primary_question': session_state['primary_question'],
+                'bot_dialogue':  session_state['bot_dialogue'],
+                'candidate_dialogue': session_state['candidate_dialogue'],
+                'chat_history' : chat_history,
+                'rationale': rationale,
+                'assessment_payload' : assessment[-1]['assessment_payload']
+                }
 
-            criteria_list=[]
-            criteria_list.append(criteria[1])
-            criteria_list.append(criteria[2])
-            hint_chain_asc=(hint_prompt_asc|llm_model)
-            #hint=await hint_chain_if_else.ainvoke({'chat_history':chat_history,'criteria': criteria_list, 'subcriteria':subcriteria_list[0:6]})
-            hint=await hint_chain_asc.ainvoke({'chat_history':chat_history, 'subcriteria':subcriteria_list[0:6]})
-            return hint.content
+    llm_response_bot_dialogue_generator = await bot_dialogue_prompt_chain.ainvoke(llm_inputs)
+    llm_content_bot_dialogue_generator = json.loads(llm_response_bot_dialogue_generator.content)
 
-        if data_structures_score < 0.4 and hint_count[1] < 2:
-            hint_count[1] += 1
-            
-            hint_chain_ds=(hint_prompt_ds|llm_model)
-            hint=await hint_chain_ds.ainvoke({'chat_history':chat_history, 'subcriteria':subcriteria_list[6:9]})
-            #hint=await hint_chain_if_else.ainvoke({'chat_history':chat_history,'criteria': criteria_list, 'subcriteria':subcriteria_list[6:9]})
-            
-            return hint.content
+    helper.pretty_log("CLASSIFY BOT DIALOGUE LLM OUTPUT", llm_content_bot_dialogue_generator)
 
-        if algorithm_score < 0.4 and hint_count[2] < 2:
-            hint_count[2] += 1
+    bot_dialogue = llm_content_bot_dialogue_generator['response']
+    bot_dialogue_rationale = llm_content_bot_dialogue_generator['rationale']
+    bot_dialogue_causal_subcriterion = llm_content_bot_dialogue_generator['subcriterion']
+    bot_dialogue_next_action = llm_content_bot_dialogue_generator['next_action']
 
-            criteria_list=[]
-            criteria_list.append(criteria[4])
+    session_state['bot_dialogue'] = bot_dialogue
+    chat_history[-1]['bot_dialogue'] = bot_dialogue
+    session_state['next_action'] = bot_dialogue_next_action
 
-            hint_chain_a=(hint_prompt_a|llm_model)
-            hint=await hint_chain_a.ainvoke({'chat_history':chat_history,'criteria': criteria_list, 'subcriteria':subcriteria_list[9:12]})
-            
-            return hint.content
+    helper.pretty_log("session_state", session_state)
+    helper.pretty_log("chat_history", chat_history)
+    helper.pretty_log("assessment", assessment)
 
-        if time_complexity_score < 0.4 and hint_count[3] < 2:
-            hint_count[3] += 1
+    logger.info("\n\n>>>>>>>>>>>FUNCTION EXIT [bot_dialogue_generator] >>>>>>>>>>>>>>>>>>>>>>>>>>\n\n")
+    return bot_dialogue_rationale, bot_dialogue_causal_subcriterion
 
-            criteria_list=[]
-            criteria_list.append(criteria[5])
-            
-            hint_chain_tc=(hint_prompt_tc|llm_model)
-            hint=await hint_chain_tc.ainvoke({'chat_history':chat_history,'criteria': criteria_list, 'subcriteria':subcriteria_list[13:16]})
-            
-            return hint.content
-
-        if space_complexity_score < 0.4 and hint_count[4] < 2:
-            hint_count[4] += 1
-
-            criteria_list=[]
-            criteria_list.append(criteria[6])
-            
-            hint_chain_sc=(hint_prompt_sc|llm_model)
-            hint=await hint_chain_sc.ainvoke({'chat_history':chat_history,'criteria': criteria_list, 'subcriteria':subcriteria_list[15:18]})
-        
-            return hint.content
-            
-
-        hint_chain=(hint_prompt|llm_model)
-        hint=await hint_chain.ainvoke({'chat_history':chat_history,'answer_evaluation':answer_evaluation})
-        return hint.content
