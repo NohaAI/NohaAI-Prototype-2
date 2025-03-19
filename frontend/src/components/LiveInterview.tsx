a9ee39b0f78d40256ba7210b73dca7904555d62e
Tarun-kumar-singh <tarunks2512@gmail.com>
2025-03-20 00:04:10 +0530

    feat: update LiveInterview component to display Mic and MicOff icons based on recording state

--- frontend/src/components/LiveInterview.tsx
+++ frontend/src/components/LiveInterview.tsx
@@ -89,6 +89,11 @@ const LiveInterview = ({ name, onCancelCall, isRecording, stopRecording, startRe
 
       {/* User Video */}
       <div className="relative bg-[#1F1F1F] rounded-lg p-4 flex flex-col justify-center items-center w-full md:w-[474px] md:h-[458px]">
+        {isRecording ? 
+          <Mic color="white" size={20} className="absolute top-2 right-2"/>
+            :
+          <MicOff color="white" size={20} className="absolute top-2 right-2"/>
+        }
         <video ref={videoRef} autoPlay playsInline muted className="w-full h-[90%] object-cover rounded-lg" />
         <p className="text-white mt-2 absolute left-3 bottom-2">{name}</p>
       </div>
