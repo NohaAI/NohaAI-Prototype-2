644e21013515227d1e85524f5e10dcbb73cf1de2
Tarun-kumar-singh <tarun.tarunksingh@gmail.com>
2025-03-24 23:21:24 +0530

    format

--- frontend/src/components/LiveInterview.tsx
+++ frontend/src/components/LiveInterview.tsx
@@ -112,31 +112,31 @@ const LiveInterview = ({
 
       {/* Noha AI */}
       <div className="w-[474px] h-[458px]">
-  <div className="relative bg-[#1F1F1F] rounded-lg p-4 flex flex-col justify-center items-center h-full">
-    {isAudioPlaying && <ScaleLoader color="white" className="absolute right-4 top-4" />}
+        <div className="relative bg-[#1F1F1F] rounded-lg p-4 flex flex-col justify-center items-center h-full">
+          {isAudioPlaying && <ScaleLoader color="white" className="absolute right-4 top-4" />}
 
-    {isAudioPlaying ? (
-      <video
-        autoPlay
-        loop
-        muted
-        className="w-[226px] h-[226px] object-cover rounded-lg"
-      >
-        <source src="9933-222013874_tiny.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    ) : (
-      <img src="noha.png" alt="Noha AI" className="w-[226px] h-[226px] object-cover" />
-    )}
+          {isAudioPlaying ? (
+            <video
+              autoPlay
+              loop
+              muted
+              className="w-[226px] h-[226px] object-cover rounded-lg"
+            >
+              <source src="9933-222013874_tiny.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          ) : (
+            <img src="noha.png" alt="Noha AI" className="w-[226px] h-[226px] object-cover" />
+          )}
 
-    <p className="text-white mt-2 absolute left-3 bottom-2">Noha</p>
-  </div>
+          <p className="text-white mt-2 absolute left-3 bottom-2">Noha</p>
+        </div>
 
-  {nohaResponseProcessing && <BeatLoader color="white" className="mt-4" />}
-  {(isAudioPlaying || !nohaResponseProcessing) && (
-    <p className="mt-1 text-white">{chats[0].name === 'Noha AI' && chats[0].message}</p>
-  )}
-</div>
+        {nohaResponseProcessing && <BeatLoader color="white" className="mt-4" />}
+        {(isAudioPlaying || !nohaResponseProcessing) && (
+          <p className="mt-1 text-white">{chats[0].name === 'Noha AI' && chats[0].message}</p>
+        )}
+      </div>
 
 
       {/* Call Controls */}
