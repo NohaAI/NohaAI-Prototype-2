5e31c95aefdb06fe4ed25f5f91a5177b9004ab33
adhirathee <bytemonk@gmail.com>
2025-04-02 00:17:00 +0530

    fix: add entries to .gitignore and change host on page.tsx to test.noha.ai

--- frontend/src/app/interview/page.tsx
+++ frontend/src/app/interview/page.tsx
@@ -12,9 +12,10 @@ const MyPage = () => {
     const [details, setDetails] = useState({} as any);
     const [callEnded, setCallEnded] = useState(false);
     const [backendServiceLink] = useState(
-	"https://test.noha.ai/backend"
+		// "https://test.noha.ai/backend"
         // "http://34.47.214.185:5001"
         // "https://apis.noha.ai"
+		"http://127.0.0.1:5001"
         );
     const [userSocket, setUserSocket] = useState<any>(null);
     const [chats, setChats] = useState<Array<any>>([]);
