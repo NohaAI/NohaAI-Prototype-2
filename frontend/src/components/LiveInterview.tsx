e81dfaf1d1c7305aea391036d4c5c523cdc03f86
Tarun-kumar-singh <tarun.tarunksingh@gmail.com>
2025-04-08 17:40:31 +0530

    refactor: clean up code formatting and improve readability in LiveInterview component

--- frontend/src/components/LiveInterview.tsx
+++ frontend/src/components/LiveInterview.tsx
@@ -5,31 +5,31 @@ import { BeatLoader, ScaleLoader } from "react-spinners";
 
 
 const LiveInterview = ({ name, onCancelCall, isRecording, stopRecording, startRecording, chats, nohaResponseProcessing, isAudioPlaying, isSilence }: any) => {
-  
-  
+
+
   const videoRef = useRef<HTMLVideoElement | null>(null);
-  
-  const isRecordingRef = useRef(isRecording);  
+
+  const isRecordingRef = useRef(isRecording);
   const isAudioPlayingRef = useRef(isAudioPlaying)
 
   const videoStreamRef = useRef<MediaStream | null>(null);
   const [startSpeakHint, setStartSpeakHint] = useState(false);
   const [stopSpeakHint, setStopSpeakHint] = useState(false);
-  
+
 
   useEffect(() => {
     async function startCamera() {
       try {
         const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "user" } });
-        
+
         if (videoStreamRef.current) {
           videoStreamRef.current.getTracks().forEach(track => track.stop());
         }
-  
+
         videoStreamRef.current = stream;
         if (videoRef.current) {
           videoRef.current.srcObject = stream;
-  
+
           videoRef.current.onloadedmetadata = () => {
             videoRef?.current?.play().catch(error => console.error("Error playing video:", error));
           };
@@ -38,16 +38,16 @@ const LiveInterview = ({ name, onCancelCall, isRecording, stopRecording, startRe
         console.error("Error accessing the camera:", error);
       }
     }
-  
+
     startCamera();
-  
+
     return () => {
       if (videoStreamRef.current) {
         videoStreamRef.current.getTracks().forEach(track => track.stop());
       }
     };
   }, []);
-  
+
   useEffect(() => {
     isRecordingRef.current = isRecording;
   }, [isRecording]);
@@ -59,20 +59,20 @@ const LiveInterview = ({ name, onCancelCall, isRecording, stopRecording, startRe
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.code === "Space") {
-        event.preventDefault(); 
-        if(isAudioPlayingRef.current){
+        event.preventDefault();
+        if (isAudioPlayingRef.current) {
           return
         }
         toggleMic();
       }
     };
-  
+
     window.addEventListener("keydown", handleKeyDown);
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
   }, []);
-  
+
 
   const toggleMic = async () => {
     if (isRecordingRef.current) {
@@ -86,34 +86,36 @@ const LiveInterview = ({ name, onCancelCall, isRecording, stopRecording, startRe
 
   return (
     <div className="min-h-screen bg-black">
-      
-      <div className="px-4 flex flex-row justify-center  pt-[4%] gap-4">
+
+      <div className="px-4 flex flex-row justify-center pt-[4%] gap-4">
 
         {/* User Video */}
-        <div className="relative bg-[#1F1F1F] rounded-lg p-4 flex flex-col justify-center items-center md:w-[474px] md:h-[458px]">
-          {isRecording ? 
-            <Mic color="white" size={20} className="absolute top-2 right-2"/>
-              :
-            <MicOff color="white" size={20} className="absolute top-2 right-2"/>
+        <div className="relative bg-[#1F1F1F] rounded-lg p-4 flex flex-col justify-center items-center md:w-[474px] h-[45vh]">
+          {isRecording ?
+            <Mic color="white" size={20} className="absolute top-2 right-2" />
+            :
+            <MicOff color="white" size={20} className="absolute top-2 right-2" />
           }
           <video ref={videoRef} autoPlay playsInline muted className="w-full h-[90%] object-cover rounded-lg" />
           <p className="text-white mt-2 absolute left-3 bottom-2">{name}</p>
         </div>
 
         {/* Noha AI */}
-        <div className="w-[474px] h-[458px]">
+        <div className="md:w-[474px] h-[45vh]">
           <div className="relative bg-[#1F1F1F] rounded-lg p-4 flex flex-col justify-center items-center h-full">
             {isAudioPlaying && <ScaleLoader color="white" className="absolute right-4 top-4" />}
             <img src="noha.png" alt="Noha AI" className="w-[226px] h-[226px] object-cover" />
             <p className="text-white mt-2 absolute left-3 bottom-2">Noha</p>
           </div>
-        </div> 
+        </div>
+
       </div>
 
-      <div className="bg-gray-800 mx-auto mt-2 w-[950px] h-[100px] z-10 flex justify-center items-center text-center p-1 border border-gray-200 rounded-lg">
+      <div className="bg-gray-800 mx-auto mt-2 w-[950px] h-[15vh] z-10 flex justify-center items-center text-center p-1 border border-gray-200 rounded-lg">
         {nohaResponseProcessing && <BeatLoader color="white" className="mt-4" />}
-        {(isAudioPlaying || !nohaResponseProcessing) && <p className="mt-1 text-white text-md">{chats[0].name === 'Noha AI' && chats[0].message}</p>}
+        {(isAudioPlaying || !nohaResponseProcessing) && <p className="mt-1 text-white text-sm">{chats[0].name === 'Noha AI' && chats[0].message}</p>}
       </div>
+
 
       {/* Call Controls */}
       <div className="fixed bottom-6 left-1/2 transform -translate-x-1/2 w-[326px]">
@@ -131,7 +133,7 @@ const LiveInterview = ({ name, onCancelCall, isRecording, stopRecording, startRe
           >
             <div className="flex flex-1 items-center justify-center">
               <Mic className="w-6 h-6" />
-            <span className="font-semibold text-lg ml-2">Talking</span>
+              <span className="font-semibold text-lg ml-2">Talking</span>
             </div>
             <div className="absolute right-2">
               {isSilence !== null && !isSilence && <ScaleLoader color="white" height={"12px"} />}
